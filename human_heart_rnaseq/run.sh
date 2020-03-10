a6dbef270e8410d2ea511f008353046bf7f2f631
Lindsey Joe Broadwell <libr8211@colorado.edu>
2020-03-10 15:07:19 -0600

    Fixed profile line to call the singularity container.

--- human_heart_rnaseq/run.sh
+++ human_heart_rnaseq/run.sh
@@ -14,6 +14,7 @@ echo "You've requested $SLURM_CPUS_ON_NODE core."
 module load singularity/3.1.1
 nextflow run nf-core/rnaseq -r 1.4.1 \
 -resume \
+-profile singularity \
 --singleEnd \
 --reads 'fastq/*.fastq' \
 --fasta ../../../genomes/Homo_sapiens/Gencode/v33/GRCh38.p13.genome.fa \
