a49315e187a92bbb6cb69f4e5f933e0e10ef2823
Michael Smallegan <michael.smallegan@colorado.edu>
2020-04-21 17:17:42 -0600

    Changing to getting the gencode annotation directly from the gencode website.

--- util/append_myh7b_exon8skipped.sh
+++ util/append_myh7b_exon8skipped.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cp ../../genomes/Homo_sapiens/Gencode/v33/gencode.v33.annotation.gtf .
-cat myh7b_exon7skipped.gtf >> gencode.v33.annotation.gtf
+cat myh7b_exon_skipped_transcript.txt >> gencode.v33.annotation.gtf
